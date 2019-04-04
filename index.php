627da52ce6efb72d179a01ade0741dcd011d9dfa
RasmusMS <33825914+RasmusMS@users.noreply.github.com>
2019-04-04 11:29:09 +0200

    Menu

--- index.php
+++ index.php
@@ -3,15 +3,28 @@
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
   <head>
-
-    <link rel="stylesheet" href="/vendors/bootstrap/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="author" content="Rasmus Marnil Sørensen">
+    <title>Portfolio</title>
+    <link rel="stylesheet" href="vendors/Bootstrap/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/styling.css">
   </head>
   <body>
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-dark border-bottom shadow-sm">
+      <h5 class="my-0 mr-md-auto font-weight-normal text-light">Portfolio - Rasmus Marnil Sørensen</h5>
+      <nav class="my-2 my-md-0 mr-md-3">
+        <a class="p-2 text-light" href="#">Hvem er jeg?</a>
+        <a class="p-2 text-light" href="#">Projekter</a>
+        <a class="p-2 text-light" href="#">Kontakt</a>
+        <!-- <a class="p-2 text-light" href="#">Pricing</a> -->
+      </nav>
+    </div>
     <div class="container">
     </div>
 
-    <script src="/vendors/bootstrap/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="vendors/Bootstrap/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </body>
 </html>
